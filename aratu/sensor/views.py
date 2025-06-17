446c72b8a2a6aea32e9d354db0cbfa6976fc6ca5
Pablo Monteiro <musicbass.3107@gmail.com>
2025-06-17 17:13:16 -0300

    bah tche -> inclusao logger da viewr.py do import de excel

--- aratu/sensor/views.py
+++ aratu/sensor/views.py
@@ -5,9 +5,13 @@ import os
 from django.http import JsonResponse
 from django.core.management import call_command
 
+import logging
+logger = logging.getLogger(__name__)
+
 def import_air_quality_data(request):
     try:
         call_command('import_air_quality_data')
         return JsonResponse({'status': 'success', 'message': 'Importação concluída com sucesso!'})
     except Exception as e:
-        return JsonResponse({'status': 'error', 'message': str(e)})
+        logger.exception("Erro na importação dos dados")
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
