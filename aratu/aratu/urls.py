e8805084ce6089cd2ea767d2d7ebd42eecbcc613
Kizz <kizzcrossoi@gmail.com>
2025-08-21 19:48:47 -0300

    feat adicionando previsaopor tarefa

--- aratu/aratu/urls.py
+++ aratu/aratu/urls.py
@@ -20,9 +20,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clients_profiles.urls')),  # Inclui todas as URLs do app
+    path('clients_profiles/', include('clients_profiles.urls')),  # <-- fica só esse
     path('login/', auth_views.LoginView.as_view(template_name='clients_profiles/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('sensor/', include('sensor.urls')),
-    path('clients_profiles/', include('clients_profiles.urls')),
 ]
